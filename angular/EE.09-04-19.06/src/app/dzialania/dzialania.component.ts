226523dd63af32c5578f27fc839b929c19908a79
John Doe <johndoe@example.com>
2023-05-25 14:19:30 +0200

    final EE.09-04-19.06

--- angular/EE.09-04-19.06/src/app/dzialania/dzialania.component.ts
+++ angular/EE.09-04-19.06/src/app/dzialania/dzialania.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm, FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-dzialania',
@@ -8,22 +8,30 @@ import { NgForm, FormsModule } from '@angular/forms';
 })
 export class DzialaniaComponent implements OnInit{
   result: number;
+  operator: string;
+  message: string;
 
   constructor() {
     this.result = 0;
+    this.message = '';
+    this.operator = '';
+  }
+  onItemSelector(operator: string) {
+    this.operator = operator;
   }
   onSubmit(form1: NgForm) {
-    // Przekaliśmy naszą zmienną lokalną 'f' z formularza dzięki czemu mamy dostęp do poszczególnych pól
     console.log("a: " + form1.value.a);
     console.log("b: " + form1.value.b);
-    console.log(form1.value);
+    console.log(form1.value);    
     let a = form1.value.a;
     let b = form1.value.b;
-    this.result = 0;
-    if(form1.value.o1 === "+") this.result = a + b;
-    this.result = 0;
-    if(form1.value.o2 === "-") this.result = a - b;
-    
+    if(this.operator === "+") this.result = a + b;
+    if(this.operator === "-") this.result = a - b;
+    if(this.operator === "*") this.result = a * b;
+    if(this.operator === "/") this.result = a / b;
+    if(this.operator === "^") this.result = Math.pow(a, b);
+    console.log(this.result);
+    this.message = `Wynik: ${this.result}`;
   }
   
   ngOnInit() {
