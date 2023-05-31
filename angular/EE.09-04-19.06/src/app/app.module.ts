f652d8dd11f81011016ef01ee6f0ddbe77e21d26
John Doe <johndoe@example.com>
2023-05-31 15:16:50 +0200

    EE.09-04-19.06

--- angular/EE.09-04-19.06/src/app/app.module.ts
+++ angular/EE.09-04-19.06/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ComponentsModule } from './components/components.module';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsModule } from './components/components.module';
+
 
 
 @NgModule({
